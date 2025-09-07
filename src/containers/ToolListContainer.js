ab86492abbb84435caf3154a801468a2cdd64d86
Süleyman TİMUR <slmn1905tmr@gmail.com>
2025-09-08 01:59:19 +0300

    ok

--- src/containers/ToolListContainer.js
+++ src/containers/ToolListContainer.js
@@ -1,29 +1,34 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { getTools } from "@/services/api"; // Sadece servis çağrısı
-import ToolList from "@/components/tools/ToolList"; // UI componentini import ediyoruz
+import { useState } from "react";
+import { getAllTools } from "@/services/api";
+
+import ToolList from "@/components/tools/ToolList";
 import styles from "@/style/ToolList.module.css";
 
 export default function ToolListContainer() {
   const [tools, setTools] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getTools();
-        setTools(data);
-      } catch (err) {
-        alert("Hata: " + err.message);
-      }
+  async function handleFetch() {
+    try {
+      const data = await getAllTools();
+      setTools(data);
+    } catch (err) {
+      alert("Hata: " + err.message);
     }
-    fetchData();
-  }, []);
+  }
 
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Tool List</h2>
-      <ToolList tools={tools} /> {/* datayı props olarak geçiyoruz */}
+
+      {/* Buton ekliyoruz */}
+      <button onClick={handleFetch} className={styles.fetchBtn}>
+        Araçları Getir
+      </button>
+
+      {/* Listeleme */}
+      <ToolList tools={tools} />
     </div>
   );
 }
